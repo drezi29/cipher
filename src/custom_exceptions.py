7c30640b8b819efac8131d4e18263f59ab68320b
drezi29 <julia.drezek98@gmail.com>
2023-05-23 22:43:16 +0200

    feat(cipher): base algorithm of encryption and decryption

--- src/custom_exceptions.py
+++ src/custom_exceptions.py
@@ -17,3 +17,15 @@ class AppendingTextToFileError(Exception):
 
     def __str__(self):
         return "Problem while appending text to existing file appeared"
+
+class DecryptionOfDecryptedMessageError(Exception):
+    '''Raised when for decrypted text decryption operation is selected'''
+
+    def __str__(self):
+        return "Attempting to decrypt an decrypted message"
+
+class EncryptionOfEncryptedMessageError(Exception):
+    '''Raised when for encrypted text encryption operation is selected'''
+
+    def __str__(self):
+        return "Attempting to encrypt an encrypted message"
