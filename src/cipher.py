a5140dbba0481f531982dd6e58ec7303129315f5
drezi29 <julia.drezek98@gmail.com>
2023-05-28 09:36:21 +0200

    refactor(cipher): separating shift method

--- src/cipher.py
+++ src/cipher.py
@@ -1,42 +1,50 @@
 from text import EncryptionStatus, Text
-from custom_exceptions import DecryptionOfDecryptedMessageError, EncryptionOfEncryptedMessageError
-
-ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+from custom_exceptions import (
+    DecryptionOfDecryptedMessageError,
+    EncryptionOfEncryptedMessageError,
+)
 
 
 class Cipher:
     @staticmethod
-    def encrypt(text_obj: Text) -> Text:
+    def encrypt(text_obj: Text, alphabet: str) -> Text:
         if text_obj.status == EncryptionStatus.ENCRYPTED:
             raise EncryptionOfEncryptedMessageError
         else:
-            encrypted_message: str = ''
-            allowed_signs = len(ALPHABET)
-            for char in text_obj.text:
-                if char.isupper():
-                    position: int = ALPHABET.find(char)
-                    encrypted_message += ALPHABET[(position + text_obj.rot_type) % allowed_signs]
-                elif char.islower():
-                    position: int = ALPHABET.find(char.upper())
-                    encrypted_message += ALPHABET[(position + text_obj.rot_type) % allowed_signs].lower()
-                else:
-                    encrypted_message += char
-            return Text(encrypted_message, text_obj.rot_type, EncryptionStatus.ENCRYPTED)
+            encrypted_message: str = Cipher.do_shift_in_text(
+                text_obj.text, text_obj.rot_type, alphabet
+            )
+            return Text(
+                encrypted_message, text_obj.rot_type, EncryptionStatus.ENCRYPTED.value
+            )
 
     @staticmethod
-    def decrypt(text_obj: Text) -> Text:
+    def decrypt(text_obj: Text, alphabet: str) -> Text:
         if text_obj.status == EncryptionStatus.DECRYPTED:
             raise DecryptionOfDecryptedMessageError
         else:
-            decrypted_message: str = ''
-            allowed_signs = len(ALPHABET)
-            for char in text_obj.text:
-                if char.isupper():
-                    position: int = ALPHABET.find(char)
-                    decrypted_message += ALPHABET[(position - text_obj.rot_type + allowed_signs) % allowed_signs]
-                elif char.islower():
-                    position: int = ALPHABET.find(char.upper())
-                    decrypted_message += ALPHABET[(position - text_obj.rot_type + allowed_signs) % allowed_signs].lower()
-                else:
-                    decrypted_message += char
-            return Text(decrypted_message, text_obj.rot_type, EncryptionStatus.DECRYPTED)
+            decrypted_message: str = Cipher.do_shift_in_text(
+                text_obj.text, -text_obj.rot_type, alphabet
+            )
+            return Text(
+                decrypted_message, text_obj.rot_type, EncryptionStatus.DECRYPTED.value
+            )
+
+    @staticmethod
+    def do_shift_in_text(text: str, rot_type: int, signs: str) -> str:
+        message = ''
+        amount_of_signs = len(signs)
+        for char in text:
+            if char.isupper():
+                position: int = signs.find(char)
+                message += signs[
+                    (position + rot_type + amount_of_signs) % amount_of_signs
+                ]
+            elif char.islower():
+                position: int = signs.find(char.upper())
+                message += signs[
+                    (position + rot_type + amount_of_signs) % amount_of_signs
+                ].lower()
+            else:
+                message += char
+        return message
