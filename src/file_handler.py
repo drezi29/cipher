ff7b82fdb62104ba24b74f03972c931657e003fc
Julia <105422077+drezi29@users.noreply.github.com>
2023-05-28 10:31:55 +0200

    Feature/file handler (#10)
    * feat(filehandler): add methods for reading, creating and appending text
    
    * style(file_handler): single quote

--- src/file_handler.py
+++ src/file_handler.py
@@ -7,22 +7,21 @@ class FileHandler:
 
     @staticmethod
     def read_file(file_name: str) -> str:
-        '''Takes file name, read data from file and return its content'''
+        """Takes file name, read data from file and return its content"""
 
         full_path = FileHandler.FOLDER_PATH + file_name
         try:
             with open(full_path, 'r') as file:
                 file_content = file.read()
-                print(file_content)
-                file.close()
                 return file_content
-        except IOError as e:
+        except IOError:
             raise ReadFileError
 
     @staticmethod
     def write_to_file(file_name: str, content: str):
-        '''Takes file name and content data that needs to be written to file, decides if file needs to be created
-        or data needs to be appended to the existing one'''
+        """Takes file name and content data that needs to be written to file,
+        decides if file needs to be created or data needs to be appended
+        to the existing one"""
 
         full_path = FileHandler.FOLDER_PATH + file_name
         if os.path.isfile(full_path):
@@ -32,23 +31,21 @@ class FileHandler:
 
     @staticmethod
     def _create_file(full_path: str, content: str):
-        '''Takes file name and content data, writes data to new file'''
+        """Takes file name and content data, writes data to new file"""
 
         try:
             with open(full_path, 'w') as file:
-                print('creating')
                 file.write(content)
-                file.close()
-        except IOError as e:
+        except IOError:
             raise CreateFileError
 
     @staticmethod
     def _append_file(full_path: str, content: str):
-        '''Takes file name and content data, writes data to existing file with given name'''
+        """Takes file name and content data, writes data to existing file
+        with given name"""
 
         try:
             with open(full_path, 'a') as file:
                 file.write('\n' + content)
-                file.close()
         except IOError:
             raise AppendingTextToFileError
