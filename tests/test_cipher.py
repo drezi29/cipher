7634c352447c39ebacb56790db2b8b06c84f6dc1
drezi29 <julia.drezek98@gmail.com>
2023-06-01 22:33:13 +0200

    refactor(tests): refactoring for use of the class, naming style

--- tests/test_cipher.py
+++ tests/test_cipher.py
@@ -1,4 +1,5 @@
 import pytest
+import string
 from src.cipher import Cipher, Text
 from src.text import EncryptionStatus
 from src.custom_exceptions import (
@@ -7,44 +8,39 @@ from src.custom_exceptions import (
 )
 
 
-def test_shouldHelloMessageBeJgnnq_when_encryptedWithRot2():
-    message: Text = Text('Hello', 2, EncryptionStatus.DECRYPTED.value)
-    cipher: Cipher = Cipher()
-    alphabet: str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    encrypted_message: Text = cipher.encrypt(message, alphabet)
-    assert encrypted_message.text == 'Jgnnq'
+class TestCipher:
+    def setup_method(self):
+        self.message = Text('Hello', 2, EncryptionStatus.DECRYPTED.value)
+        self.cipher = Cipher()
+        self.alphabet = string.ascii_uppercase
+        self.full_alphabet = ''.join([chr(i) for i in range(33, 127)])
 
+    def test_should_hello_message_be_jgnnq_when_encrypted_with_rot2(self):
+        encrypted_message = self.cipher.encrypt(self.message, self.alphabet)
+        assert encrypted_message.text == 'Jgnnq'
 
-def test_shouldMessageHaveChangedStatus_when_encrypted():
-    message: Text = Text('Hello', 2, EncryptionStatus.DECRYPTED.value)
-    cipher: Cipher = Cipher()
-    alphabet: str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    encrypted_message: Text = cipher.encrypt(message, alphabet)
-    assert encrypted_message.status != EncryptionStatus.DECRYPTED.value
-    assert encrypted_message.status == EncryptionStatus.ENCRYPTED.value
+    def test_should_message_have_changed_status_when_encrypted(self):
+        encrypted_message: Text = self.cipher.encrypt(self.message, self.alphabet)
+        assert encrypted_message.status != EncryptionStatus.DECRYPTED.value
+        assert encrypted_message.status == EncryptionStatus.ENCRYPTED.value
 
+    def test_should_message_be_the_same_when_encrypted_and_decrypted(self):
+        message = Text('Hello', 47, EncryptionStatus.DECRYPTED.value)
+        encrypted_message = self.cipher.encrypt(message, self.full_alphabet)
+        assert message != encrypted_message
+        decrypted_message = self.cipher.decrypt(encrypted_message, self.full_alphabet)
+        assert message == decrypted_message
 
-def test_shouldMessageBeTheSame_when_encryptedAndDecrypted():
-    message: Text = Text('Hello', 47, EncryptionStatus.DECRYPTED.value)
-    cipher: Cipher = Cipher()
-    alphabet: str = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
-    encrypted_message: Text = cipher.encrypt(message, alphabet)
-    assert message != encrypted_message
-    decrypted_message: Text = cipher.decrypt(encrypted_message, alphabet)
-    assert message == decrypted_message
+    def test_should_raise_encryption_of_encrypted_message_error_when_encryption_performed_on_encrypted_message(
+        self,
+    ):
+        message = Text('Jgnnq', 13, EncryptionStatus.ENCRYPTED.value)
+        with pytest.raises(EncryptionOfEncryptedMessageError):
+            self.cipher.encrypt(message, self.alphabet)
 
-
-def test_shouldRaiseEncryptionOfEncryptedMessageError_when_encryptionPerformedOnEncryptedMessage():
-    message: Text = Text('Jgnnq', 13, EncryptionStatus.ENCRYPTED.value)
-    cipher: Cipher = Cipher()
-    alphabet: str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    with pytest.raises(EncryptionOfEncryptedMessageError):
-        cipher.encrypt(message, alphabet)
-
-
-def test_shouldRaiseDecryptionOfDecryptedMessageError_when_decryptionPerformedOnDecryptedMessage():
-    message: Text = Text('Hello', 13, EncryptionStatus.DECRYPTED.value)
-    cipher: Cipher = Cipher()
-    alphabet: str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    with pytest.raises(DecryptionOfDecryptedMessageError):
-        cipher.decrypt(message, alphabet)
+    def test_should_raise_decryption_of_decrypted_message_error_when_decryption_performed_on_decrypted_message(
+        self,
+    ):
+        message = Text('Hello', 13, EncryptionStatus.DECRYPTED.value)
+        with pytest.raises(DecryptionOfDecryptedMessageError):
+            self.cipher.decrypt(message, self.alphabet)
--- tests/test_memory_buffer.py
+++ tests/test_memory_buffer.py
@@ -2,46 +2,39 @@ from src.memory_buffer import MemoryBuffer, Text
 from src.text import EncryptionStatus
 
 
-def test_shouldBufferSizeReturn1_when_1ElementAdded():
-    buffer: MemoryBuffer = MemoryBuffer()
-    assert buffer.size() == 0
-    text: Text = Text('Cipher', 13, EncryptionStatus.DECRYPTED.value)
-    buffer.add_to_buffer(text)
-    assert buffer.size() == 1
+class TestBuffer:
+    def setup_method(self):
+        self.buffer = MemoryBuffer()
+        self.text = Text('Cipher', 13, EncryptionStatus.DECRYPTED.value)
+        self.text2 = Text('Cipher2', 13, EncryptionStatus.DECRYPTED.value)
 
+    def test_should_buffer_size_return_1_when_1_element_added(self):
+        assert self.buffer.size() == 0
+        self.buffer.add_to_buffer(self.text)
+        assert self.buffer.size() == 1
 
-def test_shouldGetBufferListSize2_when_2ElementsAddes():
-    buffer: MemoryBuffer = MemoryBuffer()
-    text: Text = Text('Cipher', 13, EncryptionStatus.DECRYPTED.value)
-    text2: Text = Text('Cipher2', 13, EncryptionStatus.DECRYPTED.value)
-    buffer.add_to_buffer(text)
-    buffer.add_to_buffer(text2)
-    assert len(buffer.get_buffer_list()) == 2
+    def test_should_get_buffer_list_size_2_when_2_elements_addes(self):
+        self.buffer.add_to_buffer(self.text)
+        self.buffer.add_to_buffer(self.text2)
+        assert len(self.buffer.get_buffer_list()) == 2
 
+    def test_should_element_be_the_same_when_added_to_buffer_list(self):
+        message = 'Cipher'
+        self.buffer.add_to_buffer(self.text)
+        assert self.buffer.get_buffer_list()[0] == self.text
+        assert self.buffer.get_buffer_list()[0].text == message
+        assert (
+            self.buffer.get_buffer_list()[0].status == EncryptionStatus.DECRYPTED.value
+        )
+        assert self.buffer.get_buffer_list()[0].rot_type == 13
 
-def test_shouldElementBeTheSame_when_addedToBufferList():
-    buffer: MemoryBuffer = MemoryBuffer()
-    message: str = 'Cipher'
-    text: Text = Text(message, 13, EncryptionStatus.DECRYPTED.value)
-    buffer.add_to_buffer(text)
-    assert buffer.get_buffer_list()[0] == text
-    assert buffer.get_buffer_list()[0].text == message
-    assert buffer.get_buffer_list()[0].status == EncryptionStatus.DECRYPTED.value
-    assert buffer.get_buffer_list()[0].rot_type == 13
+    def test_should_buffer_be_empty_when_cleared(self):
+        self.buffer.add_to_buffer(self.text)
+        assert self.buffer.size() == 1
+        self.buffer.clear_buffer()
+        assert self.buffer.size() == 0
 
-
-def test_shouldBufferBeEmpty_when_cleared():
-    buffer: MemoryBuffer = MemoryBuffer()
-    text: Text = Text('Cipher', 13, EncryptionStatus.DECRYPTED.value)
-    buffer.add_to_buffer(text)
-    assert buffer.size() == 1
-    buffer.clear_buffer()
-    assert buffer.size() == 0
-
-
-def test_shouldElementBeTheSame_when_popped():
-    buffer: MemoryBuffer = MemoryBuffer()
-    text: Text = Text('Cipher', 13, EncryptionStatus.DECRYPTED.value)
-    buffer.add_to_buffer(text)
-    popped_element: Text = buffer.buffer_pop()
-    assert text == popped_element
+    def test_should_element_be_the_same_when_popped(self):
+        self.buffer.add_to_buffer(self.text)
+        popped_element: Text = self.buffer.buffer_pop()
+        assert self.text == popped_element
